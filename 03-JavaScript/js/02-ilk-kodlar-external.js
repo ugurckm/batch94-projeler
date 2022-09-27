f8c6b1fbcf7e8a194b5918c8c87979bccaebf33e
Ugur <you@example.com>
2022-09-27 20:28:19 +0300

    v1

---
+++ 03-JavaScript/js/02-ilk-kodlar-external.js
@@ -0,0 +1,3 @@
+console.log("Harici dosyadan geldi");
+console.log("22.08.2022 de Tekrar ediyorum dersi");
+/* 02 ye aktardık */
---
+++ 03-JavaScript/js/34-faktoriyel.js
@@ -0,0 +1,17 @@
+const hesapla = () =>{
+    const sayi = document.querySelector("#txtSayi").value;
+
+    if (!sayi || isNaN(sayi) || sayi<0) {
+        alert("Pozitif Bir Tam Sayi Giriniz");
+        return;
+
+    }
+
+    let carpim = 1;
+    for(let i = 1 ; i<=sayi; i++) {//(let i = sayi ; i>0 ; i--) seklinde de yazabilirdik
+        carpim = carpim*i;
+
+    }
+    document.querySelector("#sonuc").innerHTML = carpim;
+
+}
